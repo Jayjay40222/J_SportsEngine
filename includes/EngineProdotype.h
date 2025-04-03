a4d39db3a5b21a83df1745d1a96d00f88f96a1bc
Yafet Seyoum <yafetseyome@gmail.com>
2025-04-03 09:01:21 -0700

    Update EngineProdotype.h

--- includes/EngineProdotype.h
+++ includes/EngineProdotype.h
@@ -16,6 +16,9 @@
 #include <SDL_ttf.h>
 #include "include/stb_image.h"
 
+typedef struct SDL_Window JSE_Window;
+typedef struct SDL_Texture JSE_Object;
+
 void JSE_Init();
 void JSE_Quit();
 
@@ -24,10 +27,19 @@ typedef struct{
 	int height;
 	const char* title;
 	bool fullscreen;
-	SDL_Window* (*Create_window)(struct JSE_Window *win, int w, int h, const char* title, bool fullscreen);
-	SDL_Window* window;
+	JSE_Window* (*Create_window)(struct JSE_WindowHandler *winHandle, int w, int h, const char* title, bool fullscreen);
+	JSE_Window* window;
 }JSE_WindowHandler;
 
-void JSE_SetWindowActive(JSE_WindowHandler *winhandle);
+void JSE_SetWindowActive(JSE_WindowHandler *winHandle);
+
+typedef struct{
+	const char* path;
+	int width;
+	int height;
+	float xpos;
+	float ypos;
+	JSE_Object* (*Load_object)(struct JSE_SpriteHandler* sH, const char* path)
+}JSE_SpriteHandler;
 
 #endif
