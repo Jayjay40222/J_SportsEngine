61198e37c65b75a1a6b360b8434213072259df8f
Yafet Seyoum <yafetseyome@gmail.com>
2025-04-03 08:59:49 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include "includes/EngineProdotype.h"
+#include <stdbool.h>
 
 int main(){
+	JSE_Init();
 	
+	JSE_WindowHandle win_handle;
+	JSE_SetWindowActive(&win_handle);
+	JSE_Window* window = win_handle.Create_window(&win_handle, 500, 500, "JSE window", false);
 	
+	bool running = true;
+	while (running){
+		SDL_Event ev;
+		if (SDL_PollEvent(&ev)){
+			if (ev.type == SDL_QUIT){
+				running = false;
+			}
+		}
+	}
 	
+	JSE_Quit();
 	return 0;
 }
