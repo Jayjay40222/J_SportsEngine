0177cb3d73fcf4e2385bc011ea47e4d72d3ee0bc
Yafet Seyoum <yafetseyome@gmail.com>
2025-04-27 20:29:02 +0000

    Update EngineProdotype.c
    Fix some errors

--- includes/EngineProdotype.c
+++ includes/EngineProdotype.c
@@ -6,17 +6,17 @@ int current_window = 0;
 void JSE_Init(){
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0){ // Main library
 		printf("[ERROR]:%s", SDL_GetError());
-		system("pause");
+
 		exit(0);
 	}
 	if (!(IMG_Init(IMG_INIT_PNG) && IMG_Init(IMG_INIT_JPG))){ // PNG and JPG support
 		printf("[ERROR]:%s", IMG_GetError());
-		system("pause");
+
 		exit(0);
 	}
-	if (!(SDL_TTF())){ // Text support
+	if (!(TTF_Init())){ // Text support
 		printf("[ERROR]:%s", TTF_GetError());
-		system("pause");
+
 		exit(0);
 	}
 }
@@ -26,8 +26,7 @@ void JSE_Quit(){
 	IMG_Quit();
 	TTF_Quit();
 	
-	int win_i;
-	for (win_i=0;win_i<current_window;win_i++){
+	for (int win_i=0;win_i<current_window;win_i++){
 		if (windows[win_i] != NULL){
 			SDL_DestroyWindow(windows[win_i]);	
 			windows[win_i] = NULL;
@@ -35,29 +34,22 @@ void JSE_Quit(){
 	}
 }
 
-SDL_Window* Create_window(JSE_WindowHandler *winHandle, int w, int h, const char* title, bool fullscreen){
-	Uint32 screen_state = fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN;
+SDL_Window* Create_window(struct JSE_WindowHandler *winHandle, int w, int h, const char* title, bool fullscreen) {
+    Uint32 screen_state = fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN;
 
-	winHandle->window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTER, SDL_WINDOWPOS_CENTER, w, h, screen_state);
-	if (!winHandle->window){
-		printf("[ERROR]:%s", SDL_GetError());
-		system("pause");
-		exit(0);
-	}
-	
-	// Storing all argument data in the struct to act like a getter function
-	winHandle->width = w;
-	winHandle->height = h;
-	winHandle->title = title;
-	winHandle->fullscreen = fullscreen;
-	
-	return winHandle->window;
-	windows[current_window] = winHandle->window;
-	current_window++;
-}
+    winHandle->window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, screen_state);
+    if (!winHandle->window) {
+        printf("[ERROR]: %s", SDL_GetError());
 
-void JSE_SetWindowActive(JSE_WindowHandler *winHandle){
-	winHandle->Create_window = Create_window;
-	winHandle->window = NULL;
+        exit(0);
+    }
+    
+    // Store argument data in the struct
+    winHandle->width = w;
+    winHandle->height = h;
+    winHandle->title = title;
+    winHandle->fullscreen = fullscreen;
+    
+    return winHandle->window;
 }
 
